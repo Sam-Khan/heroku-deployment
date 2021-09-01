26b3c66b0e6b0317aa7970accbfc36e751f292d2
Cyber Punk <awais@marketlytics.com>
2021-09-01 17:23:20 +0500

    My first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from logging import debug
+from flask import Flask
+from flask.json import jsonify
+
+app = Flask(__name__)
+
+@app.route('/home')
+def main():
+    return jsonify({'hello':'world'}), 200
+
+app.run(debug=True)
