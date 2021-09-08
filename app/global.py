76f086be118790183d11e93d56fc08a54846e9e5
Cyber Punk <awais@marketlytics.com>
2021-09-09 02:04:23 +0500

    monor changes

--- app/global.py
+++
@@ -1,9 +0,0 @@
-
-todo_list = [
-    {'task_id':'123', 'task_name': 'write email',
-    'task_desc':'loream epsum for task'},
-    {'task_id':'124', 'task_name': 'write code',
-    'task_desc':'loream epsum for task'},
-    {'task_id':'125', 'task_name': 'write something',
-    'task_desc':'loream epsum for task'}
-]
